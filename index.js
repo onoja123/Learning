73145f347434421b2a24832843021015dfa81d92
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2021-02-16 22:45:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+//alert in js
 Alert("welcome to this page")
+// Print function
 Console.log("")
 //Conversion
+
 Var ibs = prompt ("weight in ibs"!)
 Var kg = ibs*0.42
 Alert("This is:"+kg+"kilogram")
-//Writing js off heart
+
+//depositing money in the bank
 Alert("welcome to firstbank")
 Var deposit = prompt("how much will you like to deposit today?")
 Alert ("you have deipisted"+ deposit )
+ 
+//Function in js
+2+3
+2>=3
+3<=4
+2===3 && 3===2
