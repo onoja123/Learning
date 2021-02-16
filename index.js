f6fd2cea3b31b120491d82e18d9fd98bffee9c0e
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2021-02-16 22:41:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ Var ibs = prompt ("weight in ibs"!)
 Var kg = ibs*0.42
 Alert("This is:"+kg+"kilogram")
 //Writing js off heart
-var 
+Alert("welcome to firstbank")
+Var deposit = prompt("how much will you like to deposit today?")
+Alert ("you have deipisted"+ deposit )
