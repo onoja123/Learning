f39a91c3661ef4a54508cf23afc2284d30e5e7cd
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2021-02-16 22:36:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 Alert("welcome to this page")
 Console.log("")
 //Conversion
-Var ibs = prompt ("what is the weight"!)
+Var ibs = prompt ("weight in ibs"!)
 Var kg = ibs*0.42
 Alert("your"+ibs+"kilogram")
 //Writing js off heart
+var 
