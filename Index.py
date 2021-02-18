7480270833893c9c36cad6fa2afed768c2a40128
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2021-02-18 22:03:58 +0100

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,8 +1,13 @@
 #how to print using python
 Print("hello world")
+#simple calculation using python
+Print(2+3)
+x = 5
+y +=5
+print(y)
 #variables in python
 age = 20
-Print(Onoja is + "age" + years old)
+Print("Onoja" is + age + "years old")
 #Getting input from users
 Name = input ("what is your name: ")
 print(Name)
